cbfee34ce13d273739f166cab83d254aef4762e9
Adi Rismawan Hendra <58852981+delkon20@users.noreply.github.com>
2023-11-16 13:23:01 +0700

    Add files via upload

---
+++ movie.php
@@ -0,0 +1,21 @@
+<?php
+//mengambil variable watch dr  alamat browser di simpan dalam variable var_judul
+$var_judul=$_GET["q"];
+$var_judul = str_replace("-"," ",$var_judul)
+//tampilkan $var_judul;
+?>
+<!doctype html>  
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Go Live Now</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://i.imgur.com/0K5mIFw.png" rel=icon type="image/x-icon"/>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="css/style.css">
+<meta content='0;url=https://15486454.blogspot.com/movie/<?php echo $var_judul;?>' http-equiv='refresh'/>
